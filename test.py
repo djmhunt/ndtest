608420a4f5d2fa767ffc1bb1f8806d837074dceb
Dominic Hunt <dominic.hunt+github@gmail.com>
2020-08-14 12:22:03 +0100

    Added a testing structure

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: Dominic Hunt
+
+The top level pytest running function
+
+See Also
+--------
+pytest
+"""
+import pytest
+
+if __name__ == '__main__':
+    pytest.main()
